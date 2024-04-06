4609607b2a3fea5801fc36c6f8b908611c263fc3
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-04-06 18:48:00 -0300

    create connection with postgres

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+// set database connection
+const { postgresConnection } = require('./src/database/db')
+postgresConnection()
+
 app.get('/', (req, res)=>{
 	return res.send('Ola Mundo')
 })
--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,26 @@
 const Pool = require('pg-pool')
 
-const pool = new Pool({
+// Config dotenv
+require('dotenv').config()
 
+
+const pool = new Pool({
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	port: process.env.POSTGRES_PORT,
+	database: process.env.POSTGRES_DATABASE,
+	host: process.env.POSTGRES_HOST,
 })
 
-const postgresConnection = ()=>{
+const postgresConnection = async ()=>{
+	try{
+		await pool.connect()
 
+		console.log('okay')
+	}catch(err){
+		console.log(err)
+		
+	}
 }
 
 module.exports = { postgresConnection }
