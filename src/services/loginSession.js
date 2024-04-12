9a2da52caa5d500f5cb4a684eebd03e784ba9266
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-04-12 16:43:07 -0300

    fix bugs and finish the service who set the session

--- src/services/loginSession.js
+++ src/services/loginSession.js
@@ -4,33 +4,33 @@ const { randomUUID } = require('node:crypto')
 
 
 const getLoginSession = async (id)=>{
-	const querysString = [
-		"SELECT user_id",
-		"SELECT session_id FROM sessions where user_id = $1",
-		"INSERT INTO sessions(session_id, user_id) VALUES($1, $2)"
-	]
-
 	const redisResult = await hget(`session:${id}`, "session_id")
 
 	// verify if exists any session in the redis
-	if(redisResult === null){
-		const { result } = await query(querysString[1], [id])
-		const session = randomUUID()
-
-		// set a session in the redis
-		sessionHset(`session:${id}`, [["session_id", session]])
+	if(redisResult !== null)
+		return { session: redisResult, where: 'redis' }
 
 
-		// verify if already exists any session, if there's, join in this if
-		if(result.rows.length !== 0)
-			return { session, where: 'postgres (already exists)' }
-
-		// if not, create a new and return it's session
-		await query(querysString[2], [session, id])
-		return { session, where: 'postgres (never exists before)' }
+	// If there's nothing on the redis do everything bellow
+	const querysString = {
+		insert: "INSERT INTO sessions(session_id, user_id) VALUES($1, $2)",
+		select: "SELECT session_id FROM sessions where user_id = $1"
 	}
 
-	return { session: null, where: 'redis?' }
+	// default return and the query's result
+	toReturn = { session: randomUUID(), where: 'postgres (first time)' }
+	const { result } = await query(querysString.select, [id])
+
+	// verify if already exists any session, if there's, just set the "toReturn" as that
+	if(result.rows.length !== 0)
+		toReturn = { session: result.rows[0].session_id, where: 'postgres (already exists)' }
+	
+	// if not, create a new session in the database (the "toReturn" will be the default)
+	else
+		await query(querysString.insert, [toReturn.session, id])
+
+	sessionHset(`session:${id}`, [["session_id", toReturn.session]])
+	return toReturn
 }
 
 module.exports = { getLoginSession }
