7b607e698d162b29acac7d6cdb86bd4b2efa1b1b
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-04-14 00:24:55 -0300

    finish test

--- src/controllers/auth/registerController.js
+++ src/controllers/auth/registerController.js
@@ -3,22 +3,24 @@ const { registerService } = require('../../services/authService')
 
 const register = async (req, res)=>{
 	const { name, password, nickname } = req.body
+	console.log(name, password, nickname)
 
 	// Simple verify
 	if(!name || !password || !nickname)
-		return res.send("Something is invalid")
+		return res.send("Something is invalid").status(400)
 
 	const result = await registerService(name, password, nickname)
 
 	if(result.err){
 		if(result.err === "Nickname already exists")
-			return res.end("Nickname already exists")
+			console.log('no')
+			return res.send("Nickname already exists").status(400)
 
 		console.log(result.err)
-		return res.send("WTF err")
+		return res.send("WTF err").status(401)
 	}
 
-	return res.send("User created, please, go to <a href='/'>home</a> and loggin")
+	return res.send("User created, please, go to <a href='/'>home</a> and loggin").status(200)
 }
 
 module.exports = register
--- tests/createUsers.js
+++ tests/createUsers.js
@@ -1,6 +1,26 @@
 require('dotenv').config()
 
-const a = fetch(`localhost:${process.env.APP_PORT_CONTAINER}/auth/register`, {method: 'post'}).then(e=>{
-	console.log(e)
-})
+const injectUsers = async ()=>{
+	const err = {}
+
+	console.time("Time to inject everything: ")
+
+	for(let i = 0; i < 10000; i++){
+		const result = await fetch("http://localhost:8080/auth/register", {
+			method: 'post',
+			headers: {
+				'Content-Type': "application/json"
+			},
+			body: JSON.stringify({name: 'lucas', password: "noreal", nickname: "lucas"+i})
+		})
+
+		if(result.status !== 200)
+			err[i] = { err: true, current: "lucas"+i }
+	}
+
+	console.timeEnd("Time to inject everything: ")
+	console.log(err)
+}
+
+injectUsers()
 
